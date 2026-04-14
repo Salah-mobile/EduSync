560b5c9baf36b898af9c21168c4f4c70f09e8b8e
Salah-mobile <tabitsalah12@gmail.com>
2026-04-14 16:28:39 +0100

    create tree classes and insrt them into the table classes

--- seed.sql
+++ seed.sql
@@ -1,2 +1,3 @@
 use edusync;
 INSERT INTO roles(label) VALUES ("admin"),("professeur"),("students");
+INSERT INTO classes (name,classroom_number) VALUES ("math",30),("CYBER SECURITY",20),("PHP",15);
